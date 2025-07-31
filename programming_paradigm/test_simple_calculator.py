3fa7d9c95215b8d835d9d45ea565ff2a456f16d0
Amr Mohamed <punish3r297@gmail.com>
2025-07-31 21:29:32 +0200

    .

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,24 +5,27 @@ class TestCalc(unittest.TestCase):
     """
     Test Cases for the Simple Calculator.
     """
+    def setUp(self):
+        self.calc = SimpleCalculator()
+    
     def test_add(self):
-        self.assertEqual(SimpleCalculator().add(5,5), 10)
-        self.assertEqual(SimpleCalculator().add(2,3), 5)
-        self.assertEqual(SimpleCalculator().add(-1,1), 0)
+        self.assertEqual(self.calc.add(5,5), 10)
+        self.assertEqual(self.calc.add(2,3), 5)
+        self.assertEqual(self.calc.add(-1,1), 0)
     
     def test_subtract(self):
-        self.assertEqual(SimpleCalculator().subtract(5,2), 3)
-        self.assertEqual(SimpleCalculator().subtract(3,2), 1)
-        self.assertEqual(SimpleCalculator().subtract(-1,1), -2)
+        self.assertEqual(self.calc.subtract(5,2), 3)
+        self.assertEqual(self.calc.subtract(3,2), 1)
+        self.assertEqual(self.calc.subtract(-1,1), -2)
     
     def test_multiply(self):
-        self.assertEqual(SimpleCalculator().multiply(5,2), 10)
-        self.assertEqual(SimpleCalculator().multiply(4,3), 12)
-        self.assertEqual(SimpleCalculator().multiply(1,2), 2)
+        self.assertEqual(self.calc.multiply(5,2), 10)
+        self.assertEqual(self.calc.multiply(4,3), 12)
+        self.assertEqual(self.calc.multiply(1,2), 2)
     
     def test_divide(self):
-        self.assertEqual(SimpleCalculator().divide(5,5), 1)
-        self.assertIsNone(SimpleCalculator().divide(10,0))
+        self.assertEqual(self.calc.divide(5,5), 1)
+        self.assertIsNone(self.calc.divide(10,0))
 
 if __name__ == '__main__':
     unittest.main()
